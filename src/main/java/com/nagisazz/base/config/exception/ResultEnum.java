6bf8ab21d2c7b751172a9814e65d00abca0b25f4
NagisaZZ <597306107@qq.com>
2022-12-03 21:55:50 +0800

    add 通用返回及异常处理

---
+++ src/main/java/com/nagisazz/base/config/exception/ResultEnum.java
@@ -0,0 +1,36 @@
+package com.nagisazz.base.config.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * http返回状态码及信息
+ */
+@Getter
+@AllArgsConstructor
+public enum ResultEnum {
+    /**
+     * 状态码
+     */
+    SUCCESS(true, 200, "操作成功！"),
+
+    FAIL(false, 500, "操作失败！"),
+
+    TOKEN_NOT_FOUND(false, 400, "无token，请重新登录"),
+
+    TOKEN_DECODE_FAIL(false, 400, "token解密失败"),
+    ;
+
+    /**
+     * 是否成功
+     */
+    private final Boolean success;
+    /**
+     * 状态码
+     */
+    private final Integer code;
+    /**
+     * 返回信息
+     */
+    private final String message;
+}
