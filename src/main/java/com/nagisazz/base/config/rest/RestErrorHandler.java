fb35ccb2dfe4cf9fd03c073ce112f90766180f19
zhushengzhe <zhushengzhe@thunisoft.com>
2022-12-03 14:20:34 +0800

    [+]fix

--- src/main/java/com/nagisazz/base/config/rest/RestErrorHandler.java
+++ src/main/java/com/nagisazz/base/config/rest/RestErrorHandler.java
@@ -19,22 +19,22 @@ public class RestErrorHandler implements ResponseErrorHandler {
         HttpStatus statusCode = HttpStatus.resolve(rawStatusCode);
         if (statusCode != null) {
             if (statusCode.is3xxRedirection()) {
-                log.error("状态码为{},服务端返回重定向了，可能是被登陆拦截", rawStatusCode);
+                log.error("状态码为{}，服务端返回重定向了，可能是被登陆拦截", rawStatusCode);
             } else if (403 == statusCode.value()) {
-                log.error("状态码为{},服务器返回客户端错误，授权不通过", rawStatusCode);
+                log.error("状态码为{}，服务器返回客户端错误，授权不通过", rawStatusCode);
             } else if (statusCode.is4xxClientError()) {
-                log.error("状态码为{},服务器返回客户端错误，可能参数异常或资源不存在", rawStatusCode);
+                log.error("状态码为{}，服务器返回客户端错误，可能参数异常或资源不存在", rawStatusCode);
             } else if (statusCode.is5xxServerError()) {
-                log.error("状态码为{},服务器返回服务端错误，可能参数异常或资源不存在", rawStatusCode);
+                log.error("状态码为{}，服务器返回服务端错误，可能参数异常或资源不存在", rawStatusCode);
             }
         } else {
             HttpStatus.Series series = HttpStatus.Series.resolve(rawStatusCode);
             if (series == HttpStatus.Series.REDIRECTION) {
-                log.error("状态码为{},服务端返回重定向了，可能是被登陆拦截", rawStatusCode);
+                log.error("状态码为{}，服务端返回重定向了，可能是被登陆拦截", rawStatusCode);
             } else if (series == HttpStatus.Series.CLIENT_ERROR) {
-                log.error("状态码为{},服务器返回客户端错误，可能参数异常或资源不存在", rawStatusCode);
+                log.error("状态码为{}，服务器返回客户端错误，可能参数异常或资源不存在", rawStatusCode);
             } else if (series == HttpStatus.Series.SERVER_ERROR) {
-                log.error("状态码为{},服务器返回服务端错误，可能服务端抛出异常", rawStatusCode);
+                log.error("状态码为{}，服务器返回服务端错误，可能服务端抛出异常", rawStatusCode);
             }
         }
         return false;
