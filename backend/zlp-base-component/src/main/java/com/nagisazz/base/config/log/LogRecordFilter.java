fd4a0b369e9a124ae47ea756ba984a543943d221
zhushengzhe <zhushengzhe@thunisoft.com>
2023-03-21 14:34:10 +0800

    fix log只记录json返回类型的response

--- backend/zlp-base-component/src/main/java/com/nagisazz/base/config/log/LogRecordFilter.java
+++ backend/zlp-base-component/src/main/java/com/nagisazz/base/config/log/LogRecordFilter.java
@@ -1,5 +1,26 @@
 package com.nagisazz.base.config.log;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+
 import com.alibaba.fastjson.JSON;
 import com.nagisazz.base.config.constants.BaseConstant;
 import com.nagisazz.base.config.log.request.LogRequestWrapper;
@@ -8,22 +29,14 @@ import com.nagisazz.base.entity.LogRecord;
 import com.nagisazz.base.property.ZlpProperties;
 import com.nagisazz.base.util.CommonWebUtil;
 import com.nagisazz.base.util.RequestUtil;
-import lombok.RequiredArgsConstructor;
-import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.MDC;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 日志过滤器
  */
+@Slf4j
 @RequiredArgsConstructor
 public class LogRecordFilter implements Filter {
 
@@ -39,7 +52,7 @@ public class LogRecordFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         String url = request.getRequestURL().toString();
         // 塞入logback日志需要的变量
@@ -75,19 +88,24 @@ public class LogRecordFilter implements Filter {
                 }
             }
         }
-        // 使用自定义包装过滤器的响应执行chain.doFilter
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        LogResponseWrapper wrapper = new LogResponseWrapper(response);
         // 过滤链继续
         String result = null;
         try {
-            if (Objects.isNull(requestWrapper)) {
-                filterChain.doFilter(servletRequest, wrapper);
-            } else {
-                filterChain.doFilter(requestWrapper, wrapper);
-            }
-            // 获取返回结果（仅json返回类型）并写回输出流
-            result = wrapper.writeResponse(response);
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            // 判断响应类型是否为json
+            boolean resWrapperFlag = "application/json".equals(response.getContentType());
+            // 如果是json类型，创建一个自定义的响应包装器
+            LogResponseWrapper wrapper = resWrapperFlag ? new LogResponseWrapper(response) : null;
+            // 获取请求对象，如果有请求包装器则使用它，否则使用原始请求对象
+            ServletRequest req = Objects.isNull(requestWrapper) ? servletRequest : requestWrapper;
+            // 获取响应对象，如果是json类型则使用响应包装器，否则使用原始响应对象
+            ServletResponse res = resWrapperFlag ? wrapper : response;
+            // 将请求和响应对象传递给过滤器链中的下一个过滤器或资源
+            filterChain.doFilter(req, res);
+            // 如果是json类型，获取响应包装器中的返回结果，并写回到输出流中
+            result = resWrapperFlag ? wrapper.writeResponse(response) : null;
+        } catch (Exception e) {
+            log.error("日志记录失败", e);
         } finally {
             if (flag) {
                 // 封装并储存LogRecord实体
